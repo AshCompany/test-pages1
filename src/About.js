74f549b77c1a37c22f252bf2913b7b7428d671c5
Ashkan Ansari <ashkan.ansari.aa@gmail.com>
2021-11-21 16:19:46 +0330

    btns added

--- src/About.js
+++ src/About.js
@@ -1,6 +1,18 @@
 const About = () => {
     return (
-        <div>about!!!</div>
+        <>
+            <div>about!!!</div>
+            <button className='btn1'>btn1</button>
+            <button className='btn2'>btn2</button>
+            <button className='btn3'>btn3</button>
+            <button className='btn4'>btn4</button>
+            <button className='btn5'>btn5</button>
+            <button className='btn6'>btn6</button>
+            <button className='btn7'>btn7</button>
+            <button className='btn8'>btn8</button>
+            <button className='btn9'>btn9</button>
+
+        </>
     )
 }
 export default About
