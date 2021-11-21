2cdabbd2eb8c58da1d3c3d1cdf4373564f9f8194
Ashkan Ansari <ashkan.ansari.aa@gmail.com>
2021-11-21 14:30:27 +0330

    react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './About';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Link to='/'>home</Link>
+        <Link to='/about'><div>about</div></Link>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <div>home</div>
+          </Route>
+        </Switch>
+      </Router>
+    </div >
   );
 }
 
